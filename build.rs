435c8bedfa6f28c4c79ba17796eaf646dd21b6e4
Caleb Cox <canac@users.noreply.github.com>
2023-05-12 07:55:32 -0500

    Optimize completions and manpage generation

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ use std::{fs, io, path::PathBuf};
 fn main() {
     // Don't rebuild when the generated completions change
     println!("cargo:rerun-if-changed=build.rs");
-    println!("cargo:rerun-if-changed=src");
+    println!("cargo:rerun-if-changed=src/cli.rs");
 
     generate_completions().unwrap();
     generate_manpage().unwrap();
