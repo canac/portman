82be781fc454552340b5772d9aa0271b44ad8de6
Caleb Cox <canac@users.noreply.github.com>
2024-03-17 15:55:43 -0500

    Remove fig completions

--- build.rs
+++ build.rs
@@ -16,7 +16,6 @@ fn main() {
 fn generate_completions() -> io::Result<()> {
     use clap_complete::generate_to;
     use clap_complete::shells::{Bash, Elvish, Fish, PowerShell, Zsh};
-    use clap_complete_fig::Fig;
 
     let cmd = &mut cli::Cli::command();
     let bin_name = String::from(cmd.get_name());
@@ -25,7 +24,6 @@ fn generate_completions() -> io::Result<()> {
     fs::create_dir_all(out_dir)?;
     generate_to(Bash, cmd, &bin_name, out_dir)?;
     generate_to(Elvish, cmd, &bin_name, out_dir)?;
-    generate_to(Fig, cmd, &bin_name, out_dir)?;
     generate_to(Fish, cmd, &bin_name, out_dir)?;
     generate_to(PowerShell, cmd, &bin_name, out_dir)?;
     generate_to(Zsh, cmd, &bin_name, out_dir)?;
