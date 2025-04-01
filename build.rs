2e9bb37fbddf0364fe203041f969313d99c8480a
Caleb Cox <canac@users.noreply.github.com>
2025-04-01 07:56:37 -0500

    Remove unwraps in build script

--- build.rs
+++ build.rs
@@ -2,26 +2,31 @@
 mod cli;
 
 use clap::CommandFactory;
-use std::{fs, io, path::PathBuf};
+use cli::Cli;
+use std::fs::{create_dir_all, write};
+use std::io::Result;
+use std::path::PathBuf;
 
-fn main() {
+fn main() -> Result<()> {
     // Don't rebuild when the generated completions change
     println!("cargo:rerun-if-changed=build.rs");
     println!("cargo:rerun-if-changed=src/cli.rs");
 
-    generate_completions().unwrap();
-    generate_manpage().unwrap();
+    generate_completions()?;
+    generate_manpage()?;
+
+    Ok(())
 }
 
-fn generate_completions() -> io::Result<()> {
+fn generate_completions() -> Result<()> {
     use clap_complete::generate_to;
     use clap_complete::shells::{Bash, Elvish, Fish, PowerShell, Zsh};
 
-    let cmd = &mut cli::Cli::command();
+    let cmd = &mut Cli::command();
     let bin_name = String::from(cmd.get_name());
     let out_dir = &PathBuf::from("contrib/completions");
 
-    fs::create_dir_all(out_dir)?;
+    create_dir_all(out_dir)?;
     generate_to(Bash, cmd, &bin_name, out_dir)?;
     generate_to(Elvish, cmd, &bin_name, out_dir)?;
     generate_to(Fish, cmd, &bin_name, out_dir)?;
@@ -31,17 +36,17 @@ fn generate_completions() -> io::Result<()> {
     Ok(())
 }
 
-fn generate_manpage() -> io::Result<()> {
+fn generate_manpage() -> Result<()> {
     let out_dir = &PathBuf::from("man/man1");
-    fs::create_dir_all(out_dir)?;
+    create_dir_all(out_dir)?;
 
-    let mut cmd = cli::Cli::command();
+    let mut cmd = Cli::command();
     cmd.build();
 
     let name = String::from(cmd.get_name());
     let mut buffer: Vec<u8> = Default::default();
     clap_mangen::Man::new(cmd.clone()).render(&mut buffer)?;
-    fs::write(out_dir.join(format!("{name}.1")), buffer)?;
+    write(out_dir.join(format!("{name}.1")), buffer)?;
 
     Ok(())
 }
